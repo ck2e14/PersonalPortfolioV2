5e7e4addfce9a268b6a4009fb9476057631c2dee
ck2e14 <chriswkennedy@icloud.com>
2020-07-11 10:51:39 +0100

    fixed broken resume link, styled container better and resized

--- portfoliov2/src/Components/ResumeContainer/ResumeContainer.js
+++ portfoliov2/src/Components/ResumeContainer/ResumeContainer.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import './Resume-container-style.css'
 
-const file ='https://filedn.com/lT4OmM0YRVMziUYigTE1Ok5/LatestResume2020.pdf'
+const file ='http://u.pc.cd/I4rrtalK'
+const newFile = 'https://drive.google.com/uc?export=download&id=1oAQARQrvSRi9tr_aWrOFow-IrSdzgC4t'
 
 export default class ResumeContainer extends Component {
   render() {
     return (
-      <div className='resume-wrapper-flex'>
-        <iframe src={file} title='CK-Resume' height='750' width='750' className=""></iframe>
-      </div>
+      <>
+        <div className='resume-wrapper-flex'>
+          <iframe src={file} title='CK-Resume' height='850' width='750' className="resume-pdf"></iframe>
+        </div>
+        {/* <a href={newFile} className="resume-download">Download ResumÉ</a> */}
+      </>
     );
   }
 }
