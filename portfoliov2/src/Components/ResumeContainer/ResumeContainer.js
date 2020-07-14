a941d5eae2538c1cc72b46692a0c378d64b18c73
ck2e14 <chriswkennedy@icloud.com>
2020-07-14 21:49:45 +0100

    whitespacing

--- portfoliov2/src/Components/ResumeContainer/ResumeContainer.js
+++ portfoliov2/src/Components/ResumeContainer/ResumeContainer.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './Resume-container-style.css'
 
 const file ='https://u.pcloud.link/publink/show?code=XZuybNkZ5iogAJheh6YfWiAxVHnTy0unG7VX'
-// const newFile = 'https://drive.google.com/uc?export=download&id=1oAQARQrvSRi9tr_aWrOFow-IrSdzgC4t'
 
 export default class ResumeContainer extends Component {
   render() {
